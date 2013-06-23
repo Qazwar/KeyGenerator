8924dfa729a61816333c863516f4a3a9a8465dc8
tominokers <golmagrad@gmail.com>
2013-06-23 21:41:24 +0300

    Add tests for KeyValidator.

--- KeyGeneratorTests/KeyTests/KeyTests.cpp
+++ KeyGeneratorTests/KeyTests/KeyTests.cpp
@@ -8,6 +8,7 @@
 #include "KeyTests.h"
 
 #include <KeyImplementation/Key.h>
+#include <KeyImplementation/KeyValidator.h>
 
 using namespace InnerImplementation::KeyImplementation;
 
@@ -42,6 +43,53 @@ namespace KeyTesting
 
         ASSERT_EQ(0, key.getSymbolCount());
         ASSERT_STREQ(key, keyText.c_str());
+    }
+
+    TEST_F(KeyTests, validate_correct_key)
+    {
+        std::string keyText("A1-B2-K9-H8-W7-P3");
+        Key key(keyText);
+
+        KeyValidator validator;
+
+        ASSERT_TRUE(validator.IsCorrectLength(key));
+        ASSERT_TRUE(validator.IsCorrectSymbols(key));
+    }
+
+    TEST_F(KeyTests, validate_wrong_size_key)
+    {
+        std::vector<KeyWide> wrongKeys;
+        wrongKeys.push_back(KeyWide(L""));
+        wrongKeys.push_back(KeyWide(L"A1-A2-A3-A4-A5-A6-A7-A8-A9-A1-A2"));
+        wrongKeys.push_back(KeyWide(L"A1-1-A1"));
+        wrongKeys.push_back(KeyWide(L"A1-A11-A1"));
+
+        KeyValidatorWide validator;
+
+        for (const KeyWide &key : wrongKeys)
+        {
+            ASSERT_FALSE(validator.IsCorrectLength(key));
+        }
+    }
+
+    TEST_F(KeyTests, validate_wrong_content_key)
+    {
+        std::vector<Key> wrongKeys;
+        wrongKeys.push_back(Key("A0"));
+        wrongKeys.push_back(Key("D1"));
+        wrongKeys.push_back(Key("F1-A1"));
+        wrongKeys.push_back(Key("A1-G3"));
+        wrongKeys.push_back(Key("J5-A1-B4"));
+        wrongKeys.push_back(Key("A1-M4-U2"));
+        wrongKeys.push_back(Key("A1-Q3-A4"));
+        wrongKeys.push_back(Key("T6-V8"));
+
+        KeyValidator validator;
+
+        for (const Key &key : wrongKeys)
+        {
+            ASSERT_FALSE(validator.IsCorrectSymbols(key));
+        }
     }
 
 } /* namespace KeyTesting */
