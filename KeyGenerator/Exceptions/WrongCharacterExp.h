ef609d63305dd331957f7f9060dff64a830b4975
tominokers <golmagrad@gmail.com>
2013-06-24 02:43:11 +0300

    Add exceptions for KeyGenerator.

---
+++ KeyGenerator/Exceptions/WrongCharacterExp.h
@@ -0,0 +1,21 @@
+/*
+ * WrongCharacterExp.h
+ *
+ *  Created on: 23.06.2013
+ *      Author: grad
+ */
+
+#ifndef WRONGCHARACTEREXP_H_
+#define WRONGCHARACTEREXP_H_
+
+#include <exception>
+
+namespace BinaryStudio
+{
+
+    class WrongCharacterExp : public std::exception
+    {
+    };
+
+} /* namespace BinaryStudio */
+#endif /* WRONGCHARACTEREXP_H_ */
